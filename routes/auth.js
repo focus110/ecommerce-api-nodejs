46ee3bd9fa846dca60244fed27bae635461273f6
focus110 <iamemmaochuko@gmail.com>
2022-09-06 23:58:17 +0100

    login, decrypt

--- routes/auth.js
+++ routes/auth.js
@@ -24,4 +24,28 @@ router.post("/register", async (req, res) => {
   }
 });
 
+// login user
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    if (!user) {
+      return res.status(401).json("wrong credentials!");
+    }
+
+    const { password, ...others } = user;
+
+    // Decrypt req.password
+    const bytes = CryptoJS.AES.decrypt(password, process.env.PASS_SEC);
+    const password_ = bytes.toString(CryptoJS.enc.Utf8);
+
+    if (password_ !== req.body.password) {
+      return res.status(401).json("wrong credentials!!");
+    }
+
+    res.status(200).json(others);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
 module.exports = router;
