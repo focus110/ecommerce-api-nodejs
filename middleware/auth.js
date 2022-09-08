19ea82cb0f16186d526aa436d824c98b6670cf79
focus110 <iamemmaochuko@gmail.com>
2022-09-08 23:34:55 +0100

    update user fix

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,13 +2,28 @@ const jwt = require("jsonwebtoken");
 
 const auth = (req, res, next) => {
   const authHeader = req.headers.token;
+  console.log(req.headers.token);
   if (!authHeader) return res.status(401).json("You are not authenticated!");
-
-  jwt.verify(token, process.env.JWT_SEC, (err, user) => {
+  const token = authHeader.split(" ")[1];
+  jwt.verify(authHeader, process.env.JWT_SEC, (err, user) => {
     if (err) res.status(403).json("Token is not valid!");
     req.user = user;
     next();
   });
 };
 
-module.exports = { auth };
+const authAndCheck = (req, res, next) => {
+  auth(req, res, () => {
+    const id = req.user.id;
+    const id_ = req.params.id;
+    const isAdmin = req.user.isAdmin;
+
+    if (id === id_ || isAdmin) {
+      next();
+    } else {
+      res.status(403).json("Unauthorize");
+    }
+  });
+};
+
+module.exports = { auth, authAndCheck };
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,32 @@
 const router = require("express").Router();
-const { auth } = require("../middleware/auth");
+const CryptoJS = require("crypto-js");
+const { authAndCheck } = require("../middleware/auth");
 
-router.get("/usertest", (req, res) => {
-  res.send("user");
+const User = require("../models/User");
+
+// update user
+router.put("/:id", authAndCheck, async (req, res) => {
+  const password = req.body.password;
+
+  if (password) {
+    req.body.password = CryptoJS.AES.encrypt(
+      password,
+      process.env.PASS_SEC
+    ).toString();
+  }
+
+  try {
+    const updateUser = await User.findByIdAndUpdate(
+      req.params.id,
+      { $set: req.body },
+      { new: true }
+    );
+
+    res.status(200).json();
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
 });
 
 module.exports = router;
